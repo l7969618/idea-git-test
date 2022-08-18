7f29c7c0af20728e2b78e69969568eefd343e159
l7969618 <694816967@qq.com>
2022-08-18 19:13:13 +0800

    master分支，新增代码222

--- src/a/HelloWorld.java
+++ src/a/HelloWorld.java
@@ -3,5 +3,6 @@ package a;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("我是master分支新增代码222");
     }
 }
